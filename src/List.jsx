94561a91ba38f9fa96eb4a828cd67f0ddc4782de
ariireb <ariellemgames@gmail.com>
2025-04-03 23:41:39 -0500

    X and O alternate

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+// import ListItem from "./ListItem"
+
+// export default function List({listOfItems}, {saveData}) {
+//     return (
+//         <div>
+//             {ListOfItems.map(item) (
+//                 <ListItem text={item.text}></ListItem>
+//             )}
+//             <ListItem text="example text"/>
+//         </div>
+//     )
+// }
---
+++ src/ListItem.jsx
@@ -0,0 +1,3 @@
+// export default function ListItem({text}) { // props are passed into a function, states are internal
+//     return <div>{text}</div>
+// }
